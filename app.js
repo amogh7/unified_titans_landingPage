ee3f827467ad95cb34ca4a03f430917850ce8f33
Amogh7 <amoghsachdeva3@gmail.com>
2021-01-31 02:18:21 +0530

    error fixed

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let express=require("express");
 let app=express();
-
+let http=require("http").createServer(app);
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/public/index.html");
     // res.redirect("/index.html");
 });
+
 let port=process.env.PORT||3000;
-app.listen(port,function(){
+
+http.listen(port,function(){
 console.log("started at port 3000");
 });
