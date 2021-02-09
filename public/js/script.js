420926bd06c70722fa61b66f095073e08553874b
Amogh7 <amoghsachdeva3@gmail.com>
2021-02-09 20:33:45 +0530

    navbar added

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// let navbtn=document.querySelector(".nav-btn__links");
+// let navbar=document.querySelector(".navbar-dropdown");
+// navbtn.addEventListener("click",function(){
+//     if(navbar.classList.contains("show")){
+//         navbar.classList.remove("show");
+//     }
+// else{
+//         navbar.classList.add("show");
+//     }
+// });
