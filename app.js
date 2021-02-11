41aefa3a61ff9da6586159fdcd17b39a105385a5
Amogh7 <amoghsachdeva3@gmail.com>
2021-02-11 17:43:29 +0530

    location page added

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 let express=require("express");
 let app=express();
-let http=require("http").createServer(app);
 
+let path=require("path");
+// view engine setup
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'view'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+"/public/index.html");
+    res.render("index");
+    // res.redirect("/index.html");
+});
+app.get('/contactUs',function(req,res){
+    res.render("contactUs");
+    // res.redirect("/index.html");
+})
+app.get('/locations',function(req,res){
+    res.render("locations");
     // res.redirect("/index.html");
 });
 
 let port=process.env.PORT||3000;
 
-http.listen(port,function(){
+app.listen(port,function(){
 console.log("started at port 3000");
 });
