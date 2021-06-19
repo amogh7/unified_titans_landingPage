8cb9905a5e7f09c4e82e91d59fa1c5cae3ef3963
Amogh7 <amoghsachdeva3@gmail.com>
2021-06-20 00:37:59 +0530

    email functionality added

--- app.js
+++ app.js
@@ -3,6 +3,39 @@ let app=express();
 
 
 
+const {  GMAIL_ID , GMAIL_PW } = require("./config/secrets");
+const nodemailer=require("nodemailer");
+
+
+async function sendEmail(message) {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        auth: {
+          user: GMAIL_ID ,
+          pass: GMAIL_PW,
+        },
+      });
+  
+      let res = await transporter.sendMail({
+        from: message.from, // sender address
+        to: message.to, // list of receivers
+        subject: message.subject , // Subject line
+        text:`sent by ${message.name} and he wants to know this ${message.text}` , // plain text body
+      });
+      return res;
+    } catch (error) {
+      return error;
+    }
+  }
+   
+
+
+
+
+
+
 
 let path=require("path");
 // view engine setup
@@ -10,6 +43,7 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'view'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.use(express.json());
 
 
 
@@ -30,6 +64,24 @@ app.get('/gallery',function(req,res){
     // res.redirect("/index.html");
 });
 
+app.post("/sendEmail",async function(req,res){
+  try{
+      let {message}=req.body;
+      console.log(message);
+      let response= await sendEmail(message);
+        res.status(201).json({
+        message:"sent successfuly",
+        response,
+      })
+  }
+  catch(error){
+    res.status(501).json({
+      message:"some error occured",
+      error
+    })
+
+  } 
+})
 let port=process.env.PORT||3000;
 
 app.listen(port,function(){
--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,54 @@
-// let navbtn=document.querySelector(".nav-btn__links");
-// let navbar=document.querySelector(".navbar-dropdown");
-// navbtn.addEventListener("click",function(){
-//     if(navbar.classList.contains("show")){
-//         navbar.classList.remove("show");
-//     }
-// else{
-//         navbar.classList.add("show");
-//     }
-// });
 
-// const nodemailer=require("nodemailer");
 
-// async function sendEmail(message) {
-//     try {
-//       const transporter = nodemailer.createTransport({
-//         service: "gmail",
-//         host: "smtp.gmail.com",
-//         auth: {
-//           user: GMAIL_ID ,
-//           pass: GMAIL_PW,
-//         },
-//       });
+  let btn=document.querySelector("#btnMail");
+  let name1=document.querySelector("#Contact_name");
+  let email=document.querySelector("#Contact_email");
+  let reason=document.querySelector("#Contact_reason");
+  let query=document.querySelector("#Contact_line");
+  let valueReason=reason.options[reason.selectedIndex].value;
+
+
   
-//       let res = await transporter.sendMail({
-//         from: message.from, // sender address
-//         to: message.to, // list of receivers
-//         subject: message.subject, // Subject line
-//         text: message.text, // plain text body
-//       });
-//       return res;
-//     } catch (error) {
-//       return error;
-//     }
-//   }
 
-//   let btn=document.querySelector(".btn.btn--green.btn--animated");
+ 
+let message={};
 
-//   btn.addEventListener("click",function(){
-//       console.log("btn clicked");
-//   })
+  btn.addEventListener("click",  function(e){
+       e.preventDefault();
+       console.log("btn clicked");
+       if(name1.value && email.value && valueReason && query.value){
+          console.log(name1.value);
+     
+
+           message.from="amoghsachdeva_ramesh@srmuniv.edu.in";
+           message.to=email.value;
+           message.subject=reason.value;
+           message.text=query.value;
+           message.name=name1.value;
+       
+         
+}
+ 
+
+    console.log(message);
+
+    fetch('http://localhost:3000/sendEmail', {
+      method: 'POST',
+      body: JSON.stringify({message}),
+      headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+      }
+  })
+  .then(response => response.json())
+  .then(json => {
+      console.log(json);
+  });
+});
+
+    
+
+     
+      
+
+
 
